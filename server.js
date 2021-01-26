c58db3fc8fc2407da8b1a70d7e94ce03948da579
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2021-01-26 11:10:47 -0600

    sitemap, take 2.

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.get('/wrongest', (req, res) => {
   res.sendFile(__dirname + '/html/wrongest.html');
 });
 
+
+app.get('/sitemap', (req, res) => {
+  res.sendFile(__dirname + '/xml/sitemap.xml');
+});
+
+
+
 io.on('connection', (socket) => {
   const socketID = socket.id;
   console.log('a user connected with the ID of'+socketID);
