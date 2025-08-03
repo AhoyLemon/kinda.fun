21bbe4b4d9342926085c9d50e6dd6e609d454ac1
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2025-08-02 20:04:39 -0500

    Update _functions.js

--- src/shared/js/_functions.js
+++ src/shared/js/_functions.js
@@ -7,11 +7,7 @@ export function randomFrom(array) {
 }
 
 export function shuffle(o) {
-  for (
-    var j, x, i = o.length;
-    i;
-    j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-  );
+  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
   return o;
 }
 
