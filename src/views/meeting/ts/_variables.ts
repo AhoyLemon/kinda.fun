57f035d06908412a7eef406b1f979799ff358574
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2026-06-03 11:52:45 +0200

    Turning DevSum mode off

--- src/views/meeting/ts/_variables.ts
+++ src/views/meeting/ts/_variables.ts
@@ -1,9 +1,9 @@
 import { reactive } from "vue";
 
 export const gameName = "Let's Ruin This Meeting!";
-export const timeToScore = 10000;
-export const badGuessPenalty = 4;
-export const cardsPerPlayer = 5;
+export const timeToScore = 10000; // Time in milliseconds that players have to score points after playing a card
+export const badGuessPenalty = 4; // Number of points deducted from a player for a bad guess (i.e., when they guess a card that isn't theirs)
+export const cardsPerPlayer = 5; // Number of cards each player starts with
 
 export const game = reactive({
   roomCode: "",
@@ -40,6 +40,6 @@ export const ui = reactive({
 export const settings = reactive({
   minPlayers: 2,
   maxPlayers: 6,
-  isEventActive: true,
+  isEventActive: false,
   eventCardsPerGame: 9,
 });
